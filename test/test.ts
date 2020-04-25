6feb3cadd9b79f189506e820c0b17da95011377b
Ömer Faruk Çakı <omerfarukcaki@gmail.com>
2020-04-25 23:30:36 +0300

    add: new tests

--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import 'mocha';
-import Iprange from '../src/Iprange';
+import Iprange from '../src/index';
 
 describe('Iprange class methods', () => {
   it('should return true', () => {
@@ -22,5 +22,25 @@ describe('Iprange class methods', () => {
     const range = new Iprange([['64.18.0.5', '64.18.0.255']]);
     const result = range.checkIP('64.19.0.0');
     expect(result).to.equal(false);
+  });
+  it('should return false', () => {
+    const range = new Iprange([['64.18.0.5', '64.18.0.255']]);
+    const result = range.checkIP('65.19.0.0');
+    expect(result).to.equal(false);
+  });
+  it('should return false', () => {
+    const range = new Iprange([['216.239.32.1', '216.239.63.255']]);
+    const result = range.checkIP('216.238.62.0');
+    expect(result).to.equal(false);
+  });
+  it('should return false', () => {
+    const range = new Iprange([['216.239.32.1', '216.239.63.255']]);
+    const result = range.checkIP('217.238.62.0');
+    expect(result).to.equal(false);
+  });
+  it('should return true', () => {
+    const range = new Iprange([['0.0.0.0', '1.0.0.0']]);
+    const result = range.checkIP('0.0.0.0');
+    expect(result).to.equal(true);
   });
 });
