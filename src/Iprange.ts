8541144a4c58a092d14419aa6a2f304b8aef1092
Ömer Faruk Çakı <omerfarukcaki@gmail.com>
2020-04-25 20:02:45 +0300

    Update Ipranges Class & methods

--- src/Iprange.ts
+++ src/Iprange.ts
@@ -1,5 +1,15 @@
 export default class Iprange {
-  constructor(parameters) {
-      
+  // root array which consist of ip range arrays
+  ranges: string[];
+
+  // empty array as default cosntruction parameter
+  constructor(ranges: string[] = []) {
+    this.ranges = ranges;
   }
+
+  // add new ip range [IP_start, IP_end]
+  addRange(range: string[]): void {}
+
+  // check given IP if its in the range
+  checkIP(ip: string) {}
 }
